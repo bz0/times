2d0a65e9cf2ebc493968588a96fa048f041d3180
bz0 <bzz0217@yahoo.co.jp>
2022-01-28 12:12:33 +0900

    update: PostとUserのリレーション追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,4 +12,9 @@ class Post extends Model
     protected $guarded = [
         'id'
     ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
 }
